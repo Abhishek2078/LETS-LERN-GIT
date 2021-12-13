aec623343f36e7aa51b83032b9d7d30341c2a570
Abhishek Raj Mishra <abhishekrajmishra12345@gmail.com>
2021-12-13 22:37:50 +0530

    adding sum.java

--- sum.java
+++ sum.java
@@ -2,7 +2,7 @@ public class sum {
     public static void main(String[] args) {
         int a=5;
         int b=10;
-        System.out.println("sum is"+(a+b));
+        System.out.println("sum is===="+(a+b));
     }
     
 }
